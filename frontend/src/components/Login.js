eeb373ab74cb9223c3f90a79bfc839e148ce6921
Alex <33356751+Alex-Tomo@users.noreply.github.com>
2022-01-11 20:58:10 +0000

    frontend

---
+++ frontend/src/components/Login.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+class Login extends React.Component {
+    render() {
+        return (
+            <div id="login">
+                <label htmlFor="login-username">username</label><input name="login-username" type="text" />
+                <label htmlFor="login-password">password</label><input name="login-password" type="password" />
+                <button>Login</button>
+            </div>
+        )
+    }
+}
+
+export default Login
