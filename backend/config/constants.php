91cb08f58625ee714c1f5792d9ea4899f76acfd3
Alex <33356751+Alex-Tomo@users.noreply.github.com>
2022-03-15 21:08:36 +0000

    commit

--- backend/config/constants.php
+++ backend/config/constants.php
@@ -4,9 +4,6 @@ const BASEPATH = (
     DIRECTORY_SEPARATOR . "kv6003"
 );
 
-const USER_DATABASE = "db" . DIRECTORY_SEPARATOR . "users.sqlite";
-const COURSE_DATABASE = "db" . DIRECTORY_SEPARATOR . "courses.sqlite";
-
 const DEVELOPMENT_MODE = true;
 
 const SECRET_KEY = "F384CDDB5A28FDE348F8B37B8C5DA";
