77b8a3f3f2ac3aac411bc6eb716ba60cf17315f0
Alex <33356751+Alex-Tomo@users.noreply.github.com>
2022-04-17 12:56:55 +0100

    commit

---
+++ rasa/actions/constants.py
@@ -0,0 +1,14 @@
+"""
+List of constants used in other python files
+
+@author Alex Thompson, W19007452
+"""
+
+STUDENT_TYPE_BUTTONS = [{
+        "title": "aspiring student",
+        "payload": "/my_student_type{\"student_type\":\"aspiring\"}"
+    },
+    {
+        "title": "existing student",
+        "payload": "/my_student_type{\"student_type\":\"existing\"}"
+    }]
